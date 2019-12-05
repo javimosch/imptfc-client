e91e3caa24028e73a86bcef728ae1d0b876420ba
Javier Leandro Arancibia <arancibiajav@gmail.com>
2019-12-05 22:15:27 +0000

    Removed tournement announcement

--- src/components/ChoiceTeam.vue
+++ src/components/ChoiceTeam.vue
@@ -49,17 +49,6 @@
           <div class="notification">Il y a {{goingCount}} joueurs qui vont au prochain match</div>
         </div>
       </div>
-      <div class="columns is-multiline is-mobile is-centered">
-        <div class="column is-half">
-         <b-notification
-            style="margin-bottom:20px;"
-            type="is-info"
-            aria-close-label="Close notification"
-          >
-		Rappelez-vous que le 17 novembre, il y a un tournoi au lieu du jeu hebdomadaire.
-	 </b-notification>
-        </div>
-      </div>
     </div>
   </div>
 </template>
